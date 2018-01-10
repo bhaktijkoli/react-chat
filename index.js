f55ea57d0fdd1ce98b56a64a6b6a73a372c4463e
Bhaktij Koli <bhaktijkoli121@gmail.com>
2018-01-11 02:02:15 +0530

    Added socket

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ var path = require('path');
 
 app.use(express.static(path.join(__dirname,"public")));
 
-app.listen(3000, ()=> {
+var server = app.listen(3000, ()=> {
   console.log("Server started at port 3000");
+});
+
+var io = require('socket.io')(server);
+
+io.on('connection', function(socket) {
+  console.log('user connected');
+  socket.on('chat', function(data) {
+    io.emit('chat', data);
+  });
+
+  socket.on('disconnect', function() {
+    io.emit('chat.message', 'User has disconnected.');
+  });
 });
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 
+import io from 'socket.io-client';
+const socket = io('http://localhost:3000/');
+
 class Content extends Component {
+  constructor(props)
+  {
+    super(props);
+    this.state = {messages:[]}
+    this.listenToEvents();
+  }
   render() {
     if(this.props.name == "") {
       return(
@@ -13,13 +22,26 @@ class Content extends Component {
     else {
       return(
         <div className="input-group chat-form">
-          <input id="msg" type="text" className="form-control" name="msg" placeholder="Enter your message."/>
+          <input ref="message" type="text" className="form-control" name="msg" placeholder="Enter your message."/>
           <span className="input-group-btn">
-            <button className="btn btn-success" type="button">Send</button>
+            <button className="btn btn-success" type="button" onClick={this.sendMessage.bind(this)}>Send</button>
           </span>
         </div>
       )
     }
+  }
+  listenToEvents() {
+    socket.on('chat', (data) => {
+      this.setState({
+        messages: this.state.messages.push(data),
+      })
+    })
+  }
+  sendMessage() {
+    socket.emit('chat',{
+      user: this.props.name,
+      message: this.refs.message.value,
+    });
   }
   changeName() {
     window.changeName();
